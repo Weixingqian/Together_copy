a509fa756c8732a89f7eef1f7fca9aee22a0a510
Weixingqian <xiaoyang0408@qq.com>
2014-04-26 10:47:37 +0800

    Initial Commit

---
+++ Together/MainViewController.m
@@ -0,0 +1,43 @@
+//
+//  MainViewController.m
+//  Together
+//
+//  Created by 韦兴乾 on 14-4-26.
+//  Copyright (c) 2014年 韦兴乾. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Flipside View
+
+- (void)flipsideViewControllerDidFinish:(FlipsideViewController *)controller
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([[segue identifier] isEqualToString:@"showAlternate"]) {
+        [[segue destinationViewController] setDelegate:self];
+    }
+}
+
+@end
